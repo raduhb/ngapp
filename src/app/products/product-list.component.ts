d57efbf0313294700e3f590cac86769ddb5eee81
Radu Haidet <radu.haidet@gmail.com>
2017-11-21 02:57:00 +0200

    Communication between nested components. Service added.

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core'
 import { IProduct } from './product';
 import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
+import { ProductService } from './product.service';
+
 @Component ({
     selector: 'pm-products',
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.css']
 })
 
-export class ProductListComponent implements OnInit{
+export class ProductListComponent implements OnInit {
     pageTitle: string ='Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
@@ -24,32 +26,13 @@ export class ProductListComponent implements OnInit{
     }
 
     filteredProducts: IProduct[];
-    products: IProduct[] = [
-        {
-            "productId": 1,
-            "productName": "Leaf Rake",
-            "productCode": "GDN-0011",
-            "releaseDate": "March 19, 2016",
-            "description": "Leaf rake with 48-inch wooden handle.",
-            "price": 19.95,
-            "starRating": 3.2,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-        },
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-        }
-    ];
+    products: IProduct[] = [];
 
-    constructor() {
-        this.filteredProducts = this.products;
-        this.listFilter = 'cart';
+    constructor(private _productService: ProductService) {      
+    }
+
+    onRatingClicked(message: string): void {
+        this.pageTitle = 'Product List: ' + message;
     }
 
     performFilter(filterBy: string): IProduct[] {
@@ -63,6 +46,7 @@ export class ProductListComponent implements OnInit{
     }
 
     ngOnInit(): void {
-        console.log("In OnInit");
+        this.products = this._productService.getProducts();
+        this.filteredProducts = this.products;
     }
 }
